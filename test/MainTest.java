0161c18ffc8e9c0070479587fc604d116227f741
sergaben <gabense@hotmail.com>
2018-01-17 15:06:19 +0000

    Add new test file

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,2 +1,28 @@
+import org.junit.*;
+
 public class MainTest {
+    public MainTest(){
+
+    }
+    @BeforeClass
+    public static void setUpClass(){
+
+    }
+    @AfterClass
+    public static void tearDownClass(){
+
+    }
+    @Before
+    public void setUp(){
+
+    }
+    @After
+    public void tearDown(){
+
+    }
+    @Ignore
+    @Test
+    public void notNullMethod(){
+
+    }
 }
