84386a0ea97c44f0bb0093c50830217ad59c9cb7
sergaben <gabense@hotmail.com>
2018-01-31 20:38:09 +0000

    trying to fix some issues

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,12 @@
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
-    public static final String REGEX = "\\{";
-    public static void main(String[] args) {
-        countLines("TestCode/javaSample.txt");
+    public static void main(String[] args) throws Exception {
+
+       Count countObject = new Count("TestCode/javaSample.txt");
+
+       countObject.countLinesInTextFile();
+       countObject.countCommentsForwardSlashesInTextFile();
+       countObject.countCommentsForwardSlashesAndAsteriskInTextFile();
     }
-    public static void countLines(String filename){
 
-        Scanner scanner = null;
-        ArrayList<String> linesOfCode = new ArrayList<>();
-        ArrayList<String> linesOfComments = new ArrayList<>();
-        try{
-            scanner = new Scanner(new FileReader(filename));
-            scanner.useDelimiter(REGEX);
-            while(scanner.hasNextLine()){
-                String line = scanner.nextLine().trim();
-                if(!line.isEmpty()){
-                    if(!line.startsWith("//")){
-                        linesOfCode.add(line);
-                    }else{
-                        linesOfComments.add(line);
-                    }
 
-                }
-
-                //String anotherLine = scanner.nextLine();
-                //System.out.println("   "+ line + "     ");
-            }
-//            if(scanner.hasNextLine()){
-//                String oneline = scanner.nextLine();
-//
-//                //String anotherLine = scanner.nextLine();
-//                System.out.println("   "+ oneline + "     ");
-//            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }finally{
-            if(scanner!= null){
-                scanner.close();
-            }
-        }
-        //linesOfCode.forEach(line-> System.out.println(line));
-        System.out.println("Lines of Code: " + linesOfCode.size());
-        System.out.println("Lines of Comments: " + linesOfComments.size());
-    }
 }
