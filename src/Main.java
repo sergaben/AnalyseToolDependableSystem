115131bf5edc19e8530138fc71059a0debc0f90d
Shanice Alexis <shanicealexis@Shanice-Touchs-MacBook-Pro.local>
2018-02-07 14:03:26 +0000

    re-add main class after merge conflict nonsense >.>

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/Upload.fxml"));
+
+        Parent root = (Parent) loader.load();
+        //Controller.UploadController uc = loader.<Controller.UploadController>getController();
+
+        Scene scene = new Scene(root, 500, 550);
+        String hello = "hello";
+        stage.setTitle("Alpha Analysis");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
